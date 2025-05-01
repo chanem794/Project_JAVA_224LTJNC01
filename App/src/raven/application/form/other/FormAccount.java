c9e8dac83f875b76a49839918f10adc3fb06b862
Dinh <dinhknd3@gmail.com>
2025-05-01 16:20:12 +0700

    ThemLichTrinh

--- App/src/raven/application/form/other/FormAccount.java
+++ App/src/raven/application/form/other/FormAccount.java
@@ -31,6 +31,7 @@ public class FormAccount extends javax.swing.JPanel {
                 + "focusWidth:0");
         txtHovaten.putClientProperty(FlatClientProperties.PLACEHOLDER_TEXT, "Họ và tên");
         txtNgaySinh.putClientProperty(FlatClientProperties.PLACEHOLDER_TEXT, "Ngày sinh (DD/MM/YYYY)");
+        txtEmail.putClientProperty(FlatClientProperties.PLACEHOLDER_TEXT, "");
         chDate.setTextField(txtNgaySinh); // Liên kết DateChooser với txtNgaySinh
         chDate.setDateFormat(new java.text.SimpleDateFormat("dd/MM/yyyy")); // Định dạng ngày
     }
@@ -38,25 +39,23 @@ public class FormAccount extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        panelAccount1 = new raven.application.form.other.component.PanelAccount();
         lbTitle = new javax.swing.JLabel();
         lbHovaten = new javax.swing.JLabel();
-        txtNgaySinh = new javax.swing.JTextField();
-        lbNgaySinh = new javax.swing.JLabel();
         txtHovaten = new javax.swing.JTextField();
+        lbEmail = new javax.swing.JLabel();
+        txtEmail = new javax.swing.JTextField();
+        lbNgaySinh = new javax.swing.JLabel();
+        txtNgaySinh = new javax.swing.JTextField();
         cmdLuu = new javax.swing.JButton();
 
         lbTitle.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         lbTitle.setText("Thông tin tài khoản");
-        panelAccount1.add(lbTitle);
 
         lbHovaten.setText("Họ và tên");
-        panelAccount1.add(lbHovaten);
-        panelAccount1.add(txtNgaySinh);
+
+        lbEmail.setText("Email");
 
         lbNgaySinh.setText("Ngày sinh");
-        panelAccount1.add(lbNgaySinh);
-        panelAccount1.add(txtHovaten);
 
         cmdLuu.setText("Lưu");
         cmdLuu.addActionListener(new java.awt.event.ActionListener() {
@@ -64,23 +63,44 @@ public class FormAccount extends javax.swing.JPanel {
                 cmdLuuActionPerformed(evt);
             }
         });
-        panelAccount1.add(cmdLuu);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(162, Short.MAX_VALUE)
-                .addComponent(panelAccount1, javax.swing.GroupLayout.PREFERRED_SIZE, 400, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(157, 157, 157))
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(lbTitle, javax.swing.GroupLayout.PREFERRED_SIZE, 268, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lbHovaten, javax.swing.GroupLayout.PREFERRED_SIZE, 268, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtHovaten, javax.swing.GroupLayout.PREFERRED_SIZE, 268, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lbEmail, javax.swing.GroupLayout.PREFERRED_SIZE, 268, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtEmail, javax.swing.GroupLayout.PREFERRED_SIZE, 268, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lbNgaySinh, javax.swing.GroupLayout.PREFERRED_SIZE, 268, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtNgaySinh, javax.swing.GroupLayout.PREFERRED_SIZE, 268, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(cmdLuu, javax.swing.GroupLayout.PREFERRED_SIZE, 268, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(0, 404, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(89, 89, 89)
-                .addComponent(panelAccount1, javax.swing.GroupLayout.PREFERRED_SIZE, 300, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(132, Short.MAX_VALUE))
+                .addGap(0, 170, Short.MAX_VALUE)
+                .addComponent(lbTitle)
+                .addGap(20, 20, 20)
+                .addComponent(lbHovaten)
+                .addGap(7, 7, 7)
+                .addComponent(txtHovaten, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(15, 15, 15)
+                .addComponent(lbEmail)
+                .addGap(7, 7, 7)
+                .addComponent(txtEmail, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(lbNgaySinh)
+                .addGap(18, 18, 18)
+                .addComponent(txtNgaySinh, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(cmdLuu)
+                .addGap(0, 132, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
@@ -91,10 +111,11 @@ public class FormAccount extends javax.swing.JPanel {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton cmdLuu;
+    private javax.swing.JLabel lbEmail;
     private javax.swing.JLabel lbHovaten;
     private javax.swing.JLabel lbNgaySinh;
     private javax.swing.JLabel lbTitle;
-    private raven.application.form.other.component.PanelAccount panelAccount1;
+    private javax.swing.JTextField txtEmail;
     private javax.swing.JTextField txtHovaten;
     private javax.swing.JTextField txtNgaySinh;
     // End of variables declaration//GEN-END:variables
