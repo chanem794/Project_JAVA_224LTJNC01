2eb799e61930d29b861e7616afee3edecc5d441c
Dinh <149035922+chanem794@users.noreply.github.com>
2025-05-08 21:27:31 +0700

    Tính năng B cần 4

--- App/src/dal/ConnectStationForm.java
+++ App/src/dal/ConnectStationForm.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List; 
-import model.Xe;
+import model.Xe;   
 import model.Tuyen;
 import raven.application.form.other.StationForm;
 
