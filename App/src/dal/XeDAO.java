5350b5a2ca0a01de3a473989b6a9bb2faf5bb4dd
Dinh <149035922+chanem794@users.noreply.github.com>
2025-05-08 14:21:37 +0700

    Update XeDAO.java

--- App/src/dal/XeDAO.java
+++ App/src/dal/XeDAO.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class XeDAO {
-    private Connection connection;
+    private Connection connection;  
 
     public XeDAO() {
         this.connection = DatabaseConnection.getInstance().getConnection();
